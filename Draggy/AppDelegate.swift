88ae9d4044a536abfa17860e4eb6d19b657d0f6f
dyumin <jejni.j@gmail.com>
2021-02-18 03:16:35 +0300

    Initial Commit

---
+++ Draggy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Draggy
+//
+//  Created by El D on 18.02.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Draggy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Draggy
+//
+//  Created by El D on 18.02.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
