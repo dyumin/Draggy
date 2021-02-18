58d5439af695d1376b92f291ad39ccb4ccde2f3a
dyumin <jejni.j@gmail.com>
2021-02-18 03:23:23 +0300

    some cleanup

---
+++ Draggy/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Draggy
+//
+//  Created by El D on 18.02.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Draggy/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  Draggy
+//
+//  Created by El D on 18.02.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
