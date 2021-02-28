24130a3e7ca47d09e931915d68a6faabef017d56
dyumin <jejni.j@gmail.com>
2021-02-28 14:27:45 +0300

    Add SIGTERM handler

--- Draggy/App.swift
+++ Draggy/App.swift
@@ -10,9 +10,16 @@ import Cocoa
 class App : NSApplication
 {
     let _delegate = AppDelegate()
+    let SIGTERM_handler: DispatchSourceSignal = DispatchSource.makeSignalSource(signal: SIGTERM, queue: .main)
     
     override init() {
         super.init()
+        
+        signal(SIGTERM, SIG_IGN) // // Make sure the signal does not terminate the application.
+        SIGTERM_handler.setEventHandler { [weak self] in
+            App.shared.terminate(self)
+        }
+        SIGTERM_handler.resume()
         delegate = _delegate
     }
     
