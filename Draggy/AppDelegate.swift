6aac7a1aef664dcd7bf68e47f942389d6b216d29
dyumin <jejni.j@gmail.com>
2021-03-01 22:18:11 +0300

    Fix appearance on 11.0

--- Draggy/AppDelegate.swift
+++ Draggy/AppDelegate.swift
@@ -11,6 +11,9 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if #available(OSX 10.14, *) {
+            App.shared.appearance = NSAppearance(named: .darkAqua)
+        }
         _ = DragSessionManager.shared // init shared instance
         DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
             _ = RecentAppsManager.shared // open db connection
