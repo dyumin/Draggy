955fea5da564267a8667475947e44e5cf95b8c9e
dyumin <jejni.j@gmail.com>
2021-03-17 13:46:32 +0300

    Rename DraggyTests -> RecentAppsManagerTests

--- DraggyTests/RecentAppsManagerTests.swift
+++ DraggyTests/RecentAppsManagerTests.swift
@@ -1,5 +1,5 @@
 //
-//  DraggyTests.swift
+//  RecentAppsManagerTests.swift
 //  DraggyTests
 //
 //  Created by El D on 27.02.2021.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Draggy
 
-class DraggyTests: XCTestCase {
+class RecentAppsManagerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
